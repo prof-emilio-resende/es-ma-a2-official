29a23bb8706900fbc6d4339e613c74429a4d78a6
Emilio Resende <emilio.resende@coxautoinc.com>
2021-06-07 20:02:36 -0300

    adicionando recursos basicos

--- App.tsx
+++ App.tsx
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import img from "./assets/icon.png";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.text}>
+                <Text>
+                    Hey, select one image!
+                </Text>
+            </View>
+            <Image source={img} style={styles.image} resizeMode="contain" />
+            <View style={styles.actions}>
+                <TouchableOpacity onPress={() => alert("vai!")} style={styles.button}>
+                    <Text style={styles.buttonText}>Clique aqui!</Text>
+                </TouchableOpacity>
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        marginTop: 20,
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        alignContent: "center",
+    },
+    image: {
+        flex: 1,
+    },
+    actions: {
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: {
+      borderWidth: 1,
+      borderColor: "#cccccc",
+      padding: 10,
+      ...Platform.select({
+        ios: {
+          borderRadius: 25,
+          backgroundColor: '#cccccc',
+        },
+        android: {
+          borderRadius: 5,
+          backgroundColor: '#00aaff',
+        },
+      })
+    },
+    buttonText: {    
+      ...Platform.select({
+        ios: {
+          color: '#000',
+        },
+        android: {
+          color: '#fff',
+        },
+      })
+    },
 });
